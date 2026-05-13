8a5b118eb99983ef20ab0adafb46c6f15e2e1d8d
Archana Bavachkar <archana.bavachkar@gmail.com>
2026-05-13 17:38:41 +0530

    Connected frontend to production PocketBase

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,7 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "/hcgi/platform";
+const POCKETBASE_API_URL =
+  'https://web-production-d6416a.up.railway.app';
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
