dce697fae7260c27659590c1cdd0b2ed358d8cf6
Archana Bavachkar <archana.bavachkar@gmail.com>
2026-05-19 14:36:36 +0530

    Removed native bcrypt completely

--- apps/api/src/routes/applications.js
+++ apps/api/src/routes/applications.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import pb from '../utils/pocketbaseClient.js';
 import logger from '../utils/logger.js';
 
